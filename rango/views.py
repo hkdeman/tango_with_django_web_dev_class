126858cf805a4f1b34d5ae5c581f5e2f5b026145
Hemang Kandwal <2285702k@ad.dcs.gla.ac.uk>
2018-01-18 12:18:06 +0000

    Added templates folder and working context dictionaries

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	string = "Rango says here is the home page"
-	link = "<a href='/rango/about/'>About</a>"
-	return HttpResponse(string+"\n"+link)
+	context_dict = {"boldmessage": "Crunchy,creamy,cookie,candy,cupcake!"}
+	return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
 	string = "Rango says here is the about page"
