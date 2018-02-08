47efbc79f1e30601f016b27d5df8d5633d2aa850
hkdeman <2285702k@student.gla.ac.uk>
2018-02-08 22:50:42 +0000

    Finished chapter 9 and should pass all tests

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -29,7 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#login url
 
+LOGIN_URL = '/rango/login/'
 
 
 #Static files directory
@@ -93,6 +95,16 @@ DATABASES = {
 }
 
 
+
+# Password Hashing
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -102,6 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS':{'min_length':6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
