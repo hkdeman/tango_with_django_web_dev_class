60ef0aab048884c79e3303ab737a9183bb37e961
hkdeman <2285702k@student.gla.ac.uk>
2018-02-09 00:05:19 +0000

    Finished the Chapter 10 and all tests do pass YEAH!

--- rango/views.py
+++ rango/views.py
@@ -17,15 +17,25 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+from datetime import datetime
+
 # Create your views here.
 
 def index(request):
+	# request.session.set_test_cookie()
 	category_list = Category.objects.order_by("-likes")[:5]
 	pages_list = Page.objects.order_by("-views")[:5]
 	context_dict = {"categories":category_list,"pages":pages_list}
-	return render(request,'rango/index.html',context=context_dict)
+	
+	response = render(request,'rango/index.html',context=context_dict)
+	visitor_cookie_handler(request,response)
+
+	return response
 
 def about(request):
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
 	context_dict = {"MEDIA_URL":settings.MEDIA_URL}
 	return render(request,'rango/about.html',context=context_dict)
 
@@ -142,3 +152,17 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
+
+
+def visitor_cookie_handler(request,response):
+	visits = int(request.COOKIES.get('visits','1'))
+	last_visit_cookie = request.COOKIES.get('last_visit',str(datetime.now()))
+	last_visit_time = datatime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+	if(datatime.now() - last_visit_time).days > 0:
+		visits = visits +1
+		response.set_cookie('last_visit',str(datatime.now()))
+	else:
+		response.set_cookie('last_visit',last_visit_cookie)
+
+	response.set_cookie('visits',visits)
